1d9301e81f39fb874b1cdbd2b5f1bce5f57f3133
Ilya @ Gloryx <borodinov.ilya@gmail.com>
2023-02-26 21:49:59 +0300

    fix(processor): KSP spelling is great

--- partial-ksp/src/main/kotlin/com/github/materiiapps/partial/ksp/PartialProcessor.kt
+++ partial-ksp/src/main/kotlin/com/github/materiiapps/partial/ksp/PartialProcessor.kt
@@ -160,7 +160,7 @@ internal class PartialProcessor(
                         .builder(name, type)
                         .addAnnotations(paramAnnotations)
                         .initializer(name)
-                        .addModifiers(property.modifiers.mapNotNull { it.toKModifier() }.filterNot { it == KModifier.OVERRIDE && !((property.findOverriddee()?.parentDeclaration as? KSClassDeclaration)?.let(partialSuperclasses::contains) ?: false) })
+                        .addModifiers(property.modifiers.mapNotNull { it.toKModifier() }.filterNot { it == KModifier.OVERRIDE && !((property.findOverridee()?.parentDeclaration as? KSClassDeclaration)?.let(partialSuperclasses::contains) ?: false) })
                         .build()
                 )
                 parameters.add(
